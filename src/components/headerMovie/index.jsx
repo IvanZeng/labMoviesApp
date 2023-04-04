94679ebfeb42aaf76e1ae570ddd2e6ad5997daad
Qi Zeng <ivan_zq@foxmail.com>
2023-04-04 19:14:50 +0100

    Exercise - Show favourite icon on Movie Details page & Show movie review excerpts; Refactor Details page.

--- src/components/headerMovie/index.jsx
+++ src/components/headerMovie/index.jsx
@@ -5,6 +5,8 @@ import Paper from "@mui/material/Paper";
 import IconButton from "@mui/material/IconButton";
 import Typography from "@mui/material/Typography";
 import HomeIcon from "@mui/icons-material/Home";
+import Avatar from "@mui/material/Avatar";
+import FavoriteIcon from "@mui/icons-material/Favorite";
 
 const styles = {
     root: {  
@@ -13,11 +15,24 @@ const styles = {
     alignItems: "center",
     flexWrap: "wrap",
     padding: 1.5,
+    avatar: {
+      backgroundColor: "rgb(255, 0, 0)",
+    },
   },
 };
 
 const MovieHeader = (props) => {
   const movie = props.movie;
+  const fmovies = JSON.parse(localStorage.getItem("favourites"));
+  let favourite = false;
+    for (const element of fmovies){
+      if (movie.id == element.id){
+        favourite = true
+      }else{
+        favourite = false
+      }
+    }
+
 
   return (
     <Paper component="div" sx={styles.root}>
@@ -25,7 +40,19 @@ const MovieHeader = (props) => {
         <ArrowBackIcon color="primary" fontSize="large" />
       </IconButton>
 
-      <Typography variant="h4" component="h3">
+      <a >
+        { 
+          favourite ? (
+            <FavoriteIcon sx={styles.FavoriteIcon}>
+              <FavoriteIcon />
+            </FavoriteIcon>
+          ) : null
+        }
+
+      </a>
+      <Typography variant="h4" component="h3"
+      >
+
         {movie.title}{"   "}
         <a href={movie.homepage}>
           <HomeIcon color="primary"  fontSize="='large"/>
